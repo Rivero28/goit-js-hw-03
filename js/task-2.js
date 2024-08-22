108adf8fe59ea52d9601f4b25963bb90e492a81d
Oliwia <obrycka.oliwia@wp.pl>
2024-08-22 20:39:16 +0200

    task 2

--- js/task-2.js
+++ js/task-2.js
@@ -2,5 +2,5 @@ function makeArray(firstArray, secondArray, maxLength) {
   const thirdArray = firstArray.concat(secondArray);
   if (thirdArray.length > maxLength) {
     return thirdArray.slice(0, maxLength);
-  }
+  } else return thirdArray;
 }
